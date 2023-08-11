4d1932b41cea4b2aef4c52f30e74febd7ffc9188
DKropotov <kropotovd@gmail.com>
2023-08-11 13:29:56 +0200

    gui chat

---
+++ src/main/java/guiChat/Main.java
@@ -0,0 +1,18 @@
+package guiChat;/*
+    Main Class
+ */
+
+
+/**
+ *
+ * @author Catalin Baba
+ */
+public class Main 
+{
+    public static void main(String [] args)
+    {
+        MainFrame mainFrame = new MainFrame(); //create the MainFrame object
+        Thread thread  = new Thread(mainFrame); //run in a new thread
+        thread.start(); //start the thread
+    }
+}
